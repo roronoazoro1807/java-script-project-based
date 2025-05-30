c923aaea519da287642b582593937a20bda20186
roronoazoro1807 <lovekd72@gmail.com>
2025-05-30 16:48:09 +0530

    Add  Pokémon search app project

---
+++ pokemon-search-app/script.js
@@ -0,0 +1,40 @@
+async function searchPokemon() {
+  const name = document
+    .getElementById("pokemonInput")
+    .value.trim()
+    .toLowerCase()
+    .trim();
+
+  const query = `
+  {
+  getPokemon(pokemon: ${name}){
+  sprite
+  num
+  species
+  color
+  }
+  }
+  `;
+
+  const res = await fetch("https://graphqlpokemon.favware.tech/v8/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  });
+
+  const data = await res.json();
+  const poke = data.data.getPokemon;
+
+  if (poke) {
+    document.getElementById("pokemonResult").innerHTML = `
+    <img src="${poke.sprite}" alt="${poke.species}" />
+      <p><strong>#${poke.num}</strong> - ${poke.species}</p>
+      <p>Color: ${poke.color}</p>
+    `;
+  } else {
+    document.getElementById("pokemonResult").innerHTML =
+      "<p>Pokemon not found</p>";
+  }
+}
